46b4bfbd2804dca8d3820cd47ba2c4aca9dd4fed
Abdullah Ahmed <abdallahsherdy@gmail.com>
2026-08-20 21:21:43 +0300

    JwtService

--- clean-arch/Seatsure.Application/Security/JwtService.cs
+++ clean-arch/Seatsure.Application/Security/JwtService.cs
@@ -15,8 +15,30 @@ internal sealed class JwtService : IJWTService
     {
         _jwtoptions = jwtoptions.Value;
     }
-public (string token, DateTime ExpiresAtUtc) generateToken(User user)
+
+    public (string Token, DateTime ExpiresAtUtc) GenerateToken(User user)
     {
-        throw new NotImplementedException();
+        var expiresAtUtc = DateTime.UtcNow.AddMinutes(_options.ExpiryMinutes);
+
+        
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new(JwtRegisteredClaimNames.Email, user.Email),
+            new("role", user.Role.ToString()),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Key));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _options.Issuer,
+            audience: _options.Audience,
+            claims: claims,
+            expires: expiresAtUtc,
+            signingCredentials: credentials);
+
+        return (new JwtSecurityTokenHandler().WriteToken(token), expiresAtUtc);
     }
 }
